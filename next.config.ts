ec10bcd1262617dfa1c69559461be546ff544c90
Tatsuki <iputatsuki@gmail.com>
2025-03-27 14:07:18 +0000

    fix

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ module.exports = {
     return [
       {
         source: "/api/:path*",
-        destination: "voice-dialogue-backend-bzazcff4dmdrdvh4.japaneast-01.azurewebsites.net:path*", // Django のURL
+        destination: "https://voice-dialogue-backend-bzazcff4dmdrdvh4.japaneast-01.azurewebsites.net:path*", // Django のURL
       },
     ];
   },
